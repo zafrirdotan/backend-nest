2b706352e4f5c362effa1c28019ebc38a60e4818
Zafrir <zafrir.dotan@gmail.com>
2023-08-01 09:28:26 +0300

    serving FE

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
+import { Response } from 'express';
+
+import { join } from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) { }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('*')
+  root(@Res() res: Response) {
+    res.sendFile(join(__dirname, '..', '../frontend-angular/dist/open-ai-frontend', 'index.html'));
   }
 
   @UseGuards(AuthGuard('jwt'))
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,33 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 import * as cookieParser from 'cookie-parser';
-
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
   app.enableCors({ credentials: true, origin: 'http://localhost:4200' });
   app.use(helmet());
+  // Custom middleware that applies the CSP only to the root path
+  // app.use((req, res, next) => {
+  //   if (req.path === '/chat' || req.path === '') { // Apply only to the root path
+  //     cspOptions(req, res, next);
+  //   } else {
+  //     next();
+  //   }
+  // });
+
+  app.useStaticAssets(join(__dirname, '..', '../frontend-angular/dist/open-ai-frontend'));
   await app.listen(3000);
 }
 bootstrap();
+
+
+// Define your CSP options here
+// const cspOptions = helmet.contentSecurityPolicy({
+//   directives: {
+//     ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+//     "script-src-attr": ["'self'", "'unsafe-inline'"], // Modify this to suit your needs
+//   },
+// });
