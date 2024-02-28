b623964b5e142d4107e55f92ea16d713a6c42e02
Zafrir <zafrir.dotan@gmail.com>
2024-02-28 13:07:49 +0200

    removed everything

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConversationModule } from './conversation/conversation.module';
 import { ConfigModule } from '@nestjs/config';
 // import { MongooseModule } from '@nestjs/mongoose';
-import { GroceryBotModuleV2 } from './grocery-bot-v2/grocery-bot.module';
 
 @Module({
   imports: [
-    ConversationModule,
     ConfigModule.forRoot(),
     // MongooseModule.forRoot(
     //   process.env.MONGO_URI || 'mongodb://localhost:27017/grocery-bot',
     // ),
-    GroceryBotModuleV2,
   ],
   controllers: [AppController],
   providers: [AppService],
